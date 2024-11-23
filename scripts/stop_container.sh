92439d8e45c073f587ab5486645b553466119d3b
Festus Osayi <105089377+Festus-Osayi@users.noreply.github.com>
2024-11-23 18:14:24 -0500

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,6 +2,6 @@
 set -e
 
 # Stop the running container (if any)
-containerid=`docker ps | awk -F" " '{print$1}'`
+containerid=`docker ps | awk '{print$1}' | grep -v CONTAINER`
 docker stop $containerid
 docker rm $containerid
